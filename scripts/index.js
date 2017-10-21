848bcd421c4bcb662dd7d47d4ba93daa672087bf
Lung <lung@skaut.cz>
2017-10-21 16:59:22 +0200

    changed colors according to logo colors

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,5 @@
 function hightlightMenuItem() {
-	const el = Array.from(document.querySelectorAll('.scrollblock[id]')).find(
-		el => el.getBoundingClientRect().bottom > window.innerHeight * 0.5
-	)
+	const el = Array.from(document.querySelectorAll('.scrollblock[id]')).find(el => el.getBoundingClientRect().bottom > window.innerHeight * 0.5	)
 	Array.from(document.querySelectorAll('.topMenu a')).forEach(menuEl => {
 		menuEl.parentElement.classList.toggle(
 			'active',
