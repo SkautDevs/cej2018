53f7f9dc84d88f9b78a654d4340fd7864afca275
Lung <lung@skaut.cz>
2017-11-02 10:20:41 +0100

    untrack unnessesary files + minor linting

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,20 @@
 function hightlightMenuItem() {
-	const el = Array.from(document.querySelectorAll('.scrollblock[id]')).find(el => el.getBoundingClientRect().bottom > window.innerHeight * 0.5	)
-	Array.from(document.querySelectorAll('.topMenu a')).forEach(menuEl => {
+	const el = Array.from(document.querySelectorAll('.scrollblock[id]')).find(el = > el.getBoundingClientRect().bottom > window.innerHeight * 0.5
+)
+	Array.from(document.querySelectorAll('.topMenu a')).forEach(menuEl = > {
 		menuEl.parentElement.classList.toggle(
-			'active',
-			menuEl.getAttribute('href') == '#' + el.id
-		)
-	})
+		'active',
+		menuEl.getAttribute('href') == '#' + el.id
+	)
+})
 }
 
 document.addEventListener(
 	'scroll',
-	() => {
-		hightlightMenuItem()
-	},
-	{ passive: true }
+	() = > {
+	hightlightMenuItem()
+},
+	{passive: true}
 )
 
-hightlightMenuItem()
+hightlightMenuItem();
